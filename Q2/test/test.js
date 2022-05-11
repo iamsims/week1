0d3180acba4dfbe1166dcb0a52e1b451fe5ac804
Simran KC <simrankc@anonymB.local>
2022-05-11 22:03:44 +0545

    Submission in single commit

--- Q2/test/test.js
+++ Q2/test/test.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const fs = require("fs");
 const { groth16 } = require("snarkjs");
+const { plonk } = require("snarkjs");
 
 function unstringifyBigInts(o) {
     if ((typeof(o) == "string") && (/^[0-9]+$/.test(o) ))  {
@@ -34,10 +35,61 @@ describe("HelloWorld", function () {
     });
 
     it("Should return true for correct proof", async function () {
-        //[assignment] Add comments to explain what each line is doing
-        const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2"}, "contracts/circuits/HelloWorld/HelloWorld_js/HelloWorld.wasm","contracts/circuits/HelloWorld/circuit_final.zkey");
 
-        console.log('1x2 =',publicSignals[0]);
+//this is used to generate proof and public information using the verification key - zkey, the wasm of the compilation of the circuit, and the input file.         
+const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2"}, "contracts/circuits/HelloWorld/HelloWorld_js/HelloWorld.wasm","contracts/circuits/HelloWorld/circuit_final.zkey");
+//
+	
+        //this prints the public information which is by circuits definition the multiple of inputs.
+	console.log('1x2 =',publicSignals[0]);
+	
+
+	//converts string to Ints(big ints)
+        const editedPublicSignals = unstringifyBigInts(publicSignals);
+        const editedProof = unstringifyBigInts(proof);
+
+	//export the proof and public info in bytes format
+      	const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
+	
+        //use regex to replace brackets and then split and map the integers into Sring
+	const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+    
+	//take respective arguments
+        const a = [argv[0], argv[1]];
+
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+
+        const c = [argv[6], argv[7]];
+
+        const Input = argv.slice(8);
+
+	//the verifier should verify the proof and return true
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
+    });
+    it("Should return false for invalid proof", async function () {
+        let a = [0, 0];
+        let b = [[0, 0], [0, 0]];
+        let c = [0, 0];
+        let d = [0]
+        expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
+    });
+});
+
+
+describe("Multiplier3 with Groth16", function () {
+
+    beforeEach(async function () {
+   
+
+ Verifier = await ethers.getContractFactory("Multiplier3Verifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed(); 
+    });
+
+    it("Should return true for correct proof", async function () {
+	const { proof, publicSignals } = await groth16.fullProve({"in1":"1","in2":"2", "in3":"3"}, "contracts/circuits/Multiplier3/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3/circuit_final.zkey");
+
+        console.log('1x2x3=',publicSignals[0]);
 
         const editedPublicSignals = unstringifyBigInts(publicSignals);
         const editedProof = unstringifyBigInts(proof);
@@ -59,34 +111,44 @@ describe("HelloWorld", function () {
         let d = [0]
         expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
     });
-});
-
-
-describe("Multiplier3 with Groth16", function () {
-
-    beforeEach(async function () {
-        //[assignment] insert your script here
-    });
-
-    it("Should return true for correct proof", async function () {
-        //[assignment] insert your script here
-    });
-    it("Should return false for invalid proof", async function () {
-        //[assignment] insert your script here
-    });
+    
 });
 
 
 describe("Multiplier3 with PLONK", function () {
+beforeEach(async function () {
 
-    beforeEach(async function () {
-        //[assignment] insert your script here
+
+ Verifier = await ethers.getContractFactory("PlonkVerifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
-        //[assignment] insert your script here
+        const { proof, publicSignals } = await plonk.fullProve({"in1":"1","in2":"2", "in3":"3"}, "contracts/circuits/_plonkMultiplier3/Multiplier3_js/Multiplier3.wasm","contracts/circuits/_plonkMultiplier3/circuit_final.zkey");
+
+        console.log('1x2x3=',publicSignals[0]);
+
+        const editedPublicSignals = unstringifyBigInts(publicSignals);
+        const editedProof = unstringifyBigInts(proof);
+        const calldata = await plonk.exportSolidityCallData(editedProof, editedPublicSignals);
+
+const argv = calldata.replace(/["[\]\s]/g, "").split(',');
+	
+        const a = argv[0];
+        const b = [BigInt(argv[1]).toString()];
+
+	console.log(b)
+        expect(await verifier.verifyProof(a, b)).to.be.true;
     });
+
     it("Should return false for invalid proof", async function () {
-        //[assignment] insert your script here
+        let a = 0;
+        let b = [0]
+        expect(await verifier.verifyProof(a, b)).to.be.false;
     });
+
+
+
+
 });
--- Q3/contracts/circuits/RangeProof.circom
+++ Q3/contracts/circuits/RangeProof.circom
@@ -10,6 +10,16 @@ template RangeProof(n) {
 
     component low = LessEqThan(n);
     component high = GreaterEqThan(n);
+	
+    //lower bound has to be lower than in, so low should output 1 
+    low.in[0] <== range[0];
+    low.in[1] <== in;
 
-    // [assignment] insert your code here
+    //high should output 1, the upper bound should be greater than the number
+    high.in[0] <== range[1];
+    high.in[1] <== in;
+
+    out <== low.out*high.out ; 
+
+
 }
